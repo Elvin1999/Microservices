4480b94dfe00b8b94cf888bba3f50475935d0803
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-05-26 12:08:32 +0400

    Add project files.

---
+++ Services/ResAPI/Reservation.API.Models/ReservationDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.API.Models
+{
+    public class ReservationDTO
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public DateTime? CheckInDate { get; set; }
+        public DateTime? CheckOutDate { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public double Amount { get; set; }
+    }
+}
---
+++ Services/ResAPI/Reservation.API.Services/ReservationService.cs
@@ -0,0 +1,26 @@
+﻿using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.API.Services
+{
+    public class ReservationService : IReservationService
+    {
+        public ReservationDTO GetResByNumber(int number)
+        {
+            return new ReservationDTO
+            {
+                Number = number,
+                Amount = (new Random()).Next(1000),
+                CheckInDate = DateTime.Now,
+                CheckOutDate = DateTime.Now.AddDays(10),
+                Id = (new Random()).Next(100)    ,
+                 OrderDate = DateTime.Now.AddDays(-5),
+            };
+        }
+    }
+}
